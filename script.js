5df2b3ce11290220b6bf34e73b48c6ee1465372a
sophie <158600601+sophie-narain@users.noreply.github.com>
2025-07-16 18:46:28 +0100

    Add if statement to generate computer choice

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 console.log("Hello, World!");
 function getComputerChoice () {
     let cChoice = Math.floor((Math.random() * 3));
-    
+    console.log(cChoice);
+    if (cChoice === 0) {
+        cChoice =  "rock"
+    } else if (cChoice === 1) {
+        cChoice = "paper"
+    } else {
+        cChoice = "scissors"
+    };
     console.log(cChoice)
 }
 getComputerChoice()
